c66a94a119a08e5540e6a82e378327c6077b9981
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2018-05-05 19:34:46 -0400

    Project structure
    Init empty project

---
+++ kafka-common/src/test/java/org/tnmk/skeleton/practicespringkafkagrpc/kafkacommon/KafkaCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tnmk.skeleton.practicespringkafkagrpc.kafkacommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
