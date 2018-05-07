9407f26a67cffa2daca8c48cb6adc594ffc52ff4
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2018-05-06 22:40:13 -0400

    Add more properties for consumer configuration.

--- kafka-common/src/main/java/org/tnmk/skeleton/practicespringkafkagrpc/kafkacommon/consumer/KafkaConsumerContainerFactoryBuilder.java
+++ kafka-common/src/main/java/org/tnmk/skeleton/practicespringkafkagrpc/kafkacommon/consumer/KafkaConsumerContainerFactoryBuilder.java
@@ -5,29 +5,43 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.tnmk.skeleton.practicespringkafkagrpc.kafkacommon.serialization.protobuf.ProtobufDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class KafkaConsumerContainerFactoryBuilder<T extends GeneratedMessageV3> {
-    private KafkaConsumerProperties kafkaProperties;
+    private KafkaConsumerProperties kafkaConsumerProperties;
 
-    public KafkaConsumerContainerFactoryBuilder(KafkaConsumerProperties kafkaProperties) {
-        this.kafkaProperties = kafkaProperties;
+    public KafkaConsumerContainerFactoryBuilder(KafkaConsumerProperties kafkaConsumerProperties) {
+        this.kafkaConsumerProperties = kafkaConsumerProperties;
     }
 
     public ConcurrentKafkaListenerContainerFactory<String, T> protobufConcurrentConsumerContainerFactory(Class<T> messagePayloadType) {
-        ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(protobufConsumerFactory(kafkaProperties, messagePayloadType));
-        return factory;
+        ConcurrentKafkaListenerContainerFactory<String, T> containerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConsumerFactory<String, T> consumerFactory = protobufConsumerFactory(kafkaConsumerProperties, messagePayloadType);
+
+        containerFactory.setConsumerFactory(consumerFactory);
+
+        containerFactory.setAutoStartup(kafkaConsumerProperties.isAutoStartup());
+        containerFactory.getContainerProperties().setAckMode(kafkaConsumerProperties.getAckMode());
+
+        applyRetryConfiguration(containerFactory, kafkaConsumerProperties);
+        return containerFactory;
     }
 
     private Map<String, Object> consumerConfigs(KafkaConsumerProperties kafkaProperties) {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "json");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getAutoOffsetReset());
+
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, kafkaProperties.isAutoCommit());
+        props.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProperties.getRequestTimeout());
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, kafkaProperties.getSessionTimeout());
+
         return props;
     }
 
@@ -35,5 +49,20 @@ public class KafkaConsumerContainerFactoryBuilder<T extends GeneratedMessageV3> 
         return new DefaultKafkaConsumerFactory<>(consumerConfigs(kafkaProperties), new StringDeserializer(), new ProtobufDeserializer<>(messagePayloadType));
     }
 
+    private void applyRetryConfiguration(ConcurrentKafkaListenerContainerFactory<?, ?> factory, KafkaConsumerProperties kafkaProperties) {
+        // Overriding retry template is crucial. If not defined and manual message acknowledgement is enabled, then Spring will keep retrying the same message over and over again.
+        // This behaviour will take up 100% CPU cycle and spam the logger.
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+
+        retryPolicy.setMaxAttempts(kafkaProperties.getRetryAttempt());
+        retryTemplate.setRetryPolicy(retryPolicy);
+
+        backOffPolicy.setBackOffPeriod(kafkaProperties.getRetryBackoffPeriod());
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        factory.setRetryTemplate(retryTemplate);
+    }
 
 }
--- kafka-common/src/main/java/org/tnmk/skeleton/practicespringkafkagrpc/kafkacommon/producer/KafkaProducerProperties.java
+++ kafka-common/src/main/java/org/tnmk/skeleton/practicespringkafkagrpc/kafkacommon/producer/KafkaProducerProperties.java
@@ -2,7 +2,7 @@ package org.tnmk.skeleton.practicespringkafkagrpc.kafkacommon.producer;
 
 public class KafkaProducerProperties {
     private String bootstrapServers;
-    //TODO we will be some more information about authentication here.
+    //TODO we will need some more information about authentication here.
 
     public String getBootstrapServers() {
         return bootstrapServers;
